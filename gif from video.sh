acfd8b7b7bcab37ec478f4958ba8ef9a05a651f3
Nilesh Londhe <nilesh@cloudgeni.us>
2019-03-03 10:56:57 -0800

    edits

--- gif from video.sh
+++
@@ -1,9 +0,0 @@
-gifify restore-snapshot.mp4 -o restore-snapshot.gif
-
-
-exiftool restore-snapshot.gif \
-    -artist='Nilesh Londhe' \
-    -copyright='Copyright © 2015 Cloud Genius® https://be.a.cloudgeni.us All rights reserved.' \
-    -subject='Cloud Genius®'
-
-/Applications/Safari.app/Contents/MacOS/Safari restore-snapshot.gif
